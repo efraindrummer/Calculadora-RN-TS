c31b1f2ebe7aaf67533f4cd9bb27c9a120a2716b
Efrain May <efrain_may_75@hotmail.com>
2021-06-23 14:46:15 -0500

    diseño del boton

--- src/components/BotonCalc.tsx
+++ src/components/BotonCalc.tsx
@@ -4,16 +4,21 @@ import { styles } from '../theme/appTheme'
 
 interface Props {
     texto: string;
-    color: string;
+    color?: string;
+    ancho?: boolean;
 }
 
-export const BotonCalc = ({ texto, color = '#2D2D2D' }: Props) => {
+export const BotonCalc = ({ texto, color = '#2D2D2D', ancho = false }: Props) => {
     return (
         <View style={{
             ...styles.boton,
-            backgroundColor: color
+            backgroundColor: color,
+            width: (ancho) ? 180 : 80
         }}>
-            <Text style={styles.botonTexto}>{texto}</Text>
+            <Text style={{
+                ...styles.botonTexto,
+                color: ( color === '#9B9B9B' ) ? 'black' : 'white'
+            }}>{texto}</Text>
         </View>
     )
 }
