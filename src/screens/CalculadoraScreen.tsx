5181d62a8785c714c45091cda9a0e38022030439
Efrain May <efrain_may_75@hotmail.com>
2021-06-23 13:57:54 -0500

    estrucutra + themeStyles

---
+++ src/screens/CalculadoraScreen.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+import { styles } from '../theme/appTheme'
+
+export const CalculadoraScreen = () => {
+    return (
+        <View style={styles.calculadoraContainer}>
+            <Text style={styles.resultadoPequeno}>1,500.00</Text>
+            <Text style={styles.resultado}>1,500.00</Text>
+
+            <View>
+                {/* Boton */}
+                <View style={styles.boton}>
+                    <Text style={styles.botonTexto}>1</Text>
+                </View>
+            </View>
+        </View>
+    )
+}
---
+++ src/theme/appTheme.tsx
@@ -0,0 +1,37 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    fondo: {
+        flex: 1,
+        backgroundColor: 'black',
+    },
+    calculadoraContainer: {
+        flex: 1,
+        paddingHorizontal: 20,
+        justifyContent: 'flex-end'
+    },
+    resultado: {
+        color: 'white',
+        fontSize: 60,
+        textAlign: 'right',
+    },
+    resultadoPequeno: {
+        color: 'rgba(255,255,255,0.5)',
+        fontSize: 30,
+        textAlign: 'right',
+    },
+    boton: {
+        height: 80,
+        width: 80,
+        backgroundColor: '#333333',
+        borderRadius: 100,
+        justifyContent: 'center'
+    },
+    botonTexto: {
+        textAlign: 'center',
+        padding: 10,
+        fontSize: 30,
+        color: 'white',
+        fontWeight: '300'
+    }
+});
